565db0f60a05335f114a1f47ad55e3325751f4e3
samglbh <155912005+samglbh@users.noreply.github.com>
2024-01-10 07:09:07 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,29 +7,41 @@ radio.onReceivedValue(function (name, value) {
     }
     if (velocityx > 0) {
         if (velocityy > 0) {
-            basic.showArrow(ArrowNames.NorthEast)
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.SouthWest)
         } else if (velocityy < 0) {
-            basic.showArrow(ArrowNames.NorthWest)
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.SouthEast)
         } else {
-            basic.showArrow(ArrowNames.North)
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.South)
         }
     } else if (velocityx == 0) {
         if (velocityy > 0) {
-            basic.showArrow(ArrowNames.East)
-        } else if (velocityy < 0) {
+            basic.clearScreen()
             basic.showArrow(ArrowNames.West)
+        } else if (velocityy < 0) {
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.East)
         } else {
+            basic.clearScreen()
             basic.showIcon(IconNames.Yes)
         }
     } else {
         if (velocityy > 0) {
-            basic.showArrow(ArrowNames.SouthEast)
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.NorthWest)
         } else if (velocityy < 0) {
-            basic.showArrow(ArrowNames.SouthWest)
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.NorthEast)
         } else {
-            basic.showArrow(ArrowNames.South)
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.North)
         }
     }
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
 })
 let velocityy = 0
 let velocityx = 0
